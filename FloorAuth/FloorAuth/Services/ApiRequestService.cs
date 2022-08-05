4ca06917505b2544c34e9d383fe8093f5579a8b1
emmaofori20 <emmanuelofori2638@gmail.com>
2022-08-05 17:47:12 +0000

    first commit

---
+++ FloorAuth/FloorAuth/Services/ApiRequestService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloorAuth.Services
+{
+    class ApiRequestService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+      
+        ///Method that get all floors/////
+        public static async Task GetAllFloors(string username, string AuthKey, string timeStamp)
+        {
+            var content = "unknown error";
+
+            try
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/xml")
+                    );
+                // client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+                client.DefaultRequestHeaders.Add("ApiKey", username);
+                client.DefaultRequestHeaders.Add("Authorization", AuthKey);
+                client.DefaultRequestHeaders.Add("ts", timeStamp);
+
+                var stringTask = await client.GetStringAsync("https://assignment.enappgy.io/ems/api/org/floor/list");
+
+                Console.Write(stringTask);
+            }
+            catch (Exception ex)
+            {
+
+                content = ex.Message;
+            }
+
+        }
+
+    }
+}
